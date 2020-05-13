548fcdc286bbd9140510395e856a81a1d099dea5
Truman <truman@Trumans-iMac.local>
2020-05-12 19:56:03 -0500

    Finished MVP

--- index.js
+++ index.js
@@ -39,8 +39,21 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+Person.prototype.eat = function(someFood) {
+  if(this.stomach.length < 10){
+    this.stomach.push(someFood)
+  }
+}
+Person.prototype.poop = function(){
+  this.stomach.length = 0;
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`
 }
 
 /*
@@ -57,9 +70,16 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons
+}
+
 
 /*
   TASK 3
@@ -68,18 +88,22 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy
 }
-
+Baby.prototype = Object.create(Person.prototype)
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
+}
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window/ Global Object Binding binds "this" to the entire window.
+  2. Implicit Binding is when "this" is bound to the object before the dot.
+  3. New Binding is when "this" is bound to the object that is created by a constructor function.
+  4. Explicit binding is when we use the call or apply method in JS.
 */
 
 
